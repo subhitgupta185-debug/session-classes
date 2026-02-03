0b60a7fdd4cb0b82f3617dd7d99c2cddee69dd42
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-02-03 20:28:19 +0530

    Understanding maths

---
+++ Practicehai/math.py
@@ -0,0 +1,79 @@
+#basic maths in python.
+
+a = 10 
+b = 9 
+sum = a + b , # addition
+print(sum)
+
+
+a = 50
+b = 30
+sub = a - b # subtraction
+print(sub)
+
+
+mul = a * b  # multiplication
+print(mul)
+
+
+div = a / b # division
+print(div)
+
+
+nearest_int_div = a//b
+print(nearest_int_div)
+
+
+# 11 / 3
+# qoutient = 3 # Using // 
+# remainder = 2 # using %
+
+remainder = a % b
+print(remainder)
+
+
+c = 2
+d = 2
+# 4 is the power of 16
+exponent = c ** d
+print(exponent)
+
+
+
+
+x = 20
+y = 4
+exponent = x ** y
+print(exponent)
+
+# Examples : 
+
+#price = 50
+# quantity = 4
+
+price = 50
+quantity = 4
+total_cost = price * quantity
+print(total_cost)
+
+
+
+# Examples : 
+total_marks = 450
+subjects =  11
+average = total_marks / subjects
+quotient = total_marks // subjects
+print(average)
+print(quotient)
+
+# order of execution
+
+# 1. parenthesis.
+# 2. Exponents
+# 3. Multiplication / Divison
+# 4. Addition / Subtraction.
+
+
+# Example : 
+result = (2 + 3) * 4 + 6/2
+print(result)
