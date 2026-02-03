79ec5b430b350055866cc9f8f45dc5142b602941
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-02-03 21:08:31 +0530

    Class content

---
+++ Practicehai/abc.py
@@ -0,0 +1,4 @@
+text = 5
+text2 = '5'
+inttext1 = int(text2)
+print(inttext1)
---
+++ Practicehai/input.py
@@ -0,0 +1,23 @@
+# input () --> function basically allows to take data or input from the user...
+
+name = input("enter your name: ")
+print(f" Hello, {name}")
+
+age = input("enter your age : ")
+print(f"Hello, {name} you are {age} years old")
+
+
+
+
+
+name = input("Enter your name :")
+print(f"Hello, {name}")
+Age = input("Enter your age :")
+print(f"Hey {name}, how are you nice to heard you are {age} years old")
+
+
+
+text = 5
+text2 = '15'
+inttext1 = int(text2)
+print(inttext1)
