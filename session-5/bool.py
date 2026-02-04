28e28a5d5f9c1b2c07078e740cc3800cafd485f7
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-02-04 16:16:25 +0530

    Booleans first class concept

---
+++ session-5/bool.py
@@ -0,0 +1,29 @@
+is_logged_in = True
+
+if is_logged_in : 
+   print("Hello user")
+
+if is_logged_in == True:
+   print("Hello user")
+
+
+
+atm_pin = 1234
+entered_pin = 1111
+
+# Pin accepted or pin wrong
+
+if entered_pin == atm_pin:
+   print("Pin accepted")
+else: 
+   print("Pin wrong")
+
+
+
+is_rainy_day = True
+
+if is_rainy_day == True: #True or False True == "True"(Kabhi bhi inversed comas me nahi ho sakta...)
+   print("Yes")
+   
+
+
