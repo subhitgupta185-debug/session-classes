b274992d26048506e4b2e8f5823683b146c81c0b
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-02-05 22:42:57 +0530

    loops session

---
+++ session 6/loops.py
@@ -0,0 +1,28 @@
+# loop variable in [0, 1, 2, 3, 4]
+#for i in range(5):
+   # print(i)
+
+
+for subhit in range(5):
+    print(subhit)
+    print("subhit")
+
+
+
+
+# print from 1 to 10 :
+for i in range(1, 11):
+    print(i)
+
+# print 1, 3, 5, 7, 9 :
+for i in range(1, 11 , 2):
+    print(i)
+
+# reverse ; 5 4 3 2 1 0 -1 -2
+
+# start = 5 
+# end = -3
+# steps = -1
+
+for i in range(5, -3, -1):
+ print(i)
