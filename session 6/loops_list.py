1b01390bd300ec68619fbcc36a18f0dc1edd963d
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-02-06 09:43:52 +0530

    Loops session 6

---
+++ session 6/loops_list.py
@@ -0,0 +1,13 @@
+names = ("A", "B", "C")
+
+# range(5) = [0, 1, 2, 3, 4]
+
+                    #i    #i   #i 
+for i in names:   # ["A", "B", "C"]          [ Python allows the i or any other variable to move steps by step and print.]
+ print(i)
+
+
+# python says
+# take one name
+# print it
+# move to name
