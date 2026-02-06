10b2c78b51ee4b70bb3b71d7ad10c3536370893d
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-02-06 10:47:24 +0530

    while loops session 6

---
+++ session 6/while_loop.py
@@ -0,0 +1,18 @@
+#count = 0
+
+
+#while count < 77:  # while the condition is true keep looping , count = 0, 0 < 3
+    #print(count) # 0
+    #print("Hello") # hello
+   # count = count + 7 # count = 0 + 5 = 5
+
+
+
+# You ask user to enter password till he/she did'nt enter the correct password.
+
+actual_password = "admin123"
+
+password = ""
+while password != actual_password: #"aferd" != admin123 # == / != [ When admin123 == admin123, it becomes false so python breaks the loop.]
+    password = input("Enter your password : ")
+    
