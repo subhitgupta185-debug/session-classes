92acd53c08cf523b9dd16093c549fc2606fe98af
subhitgupta185-debug <subhitgupta185@gmail.com>
2026-03-25 00:30:31 +0530

    csv session 13

--- session - 13/reading_csv.py
+++ session - 13/reading_csv.py
@@ -1,7 +1,26 @@
 import pandas as pd
 
-df = pd.read_csv("employee.csv")
+df = pd.read_csv("abc.csv")
 print(df)
 
 
 
+# That single line :
+# opens the file
+# parese rows and columns
+# data type infer
+# constructed a dataframe
+
+val = df.head(10)
+print(val)
+
+vel = df.tail()
+print(vel)
+
+val_3 = df.iloc[2 :7]
+print(val_3)
+
+
+
+info = df.info()
+print(df.info)
